2c77675b74cbef70485c23e5fc8748b82e613570
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-24 22:12:00 +0300

    add cron validation

--- src/configSchema.ts
+++ src/configSchema.ts
@@ -1,3 +1,9 @@
+import { Validator } from 'jsonschema';
+import { isValidCron } from 'cron-validator';
+
+/**
+ * JSON schema for Cron Manager config
+ */
 const configSchema = {
   id: '/Config',
   properties: {
@@ -9,14 +15,37 @@ const configSchema = {
   required: [ 'tasks' ],
 };
 
+/**
+ * JSON Schema for specific task
+ */
 const taskSchema = {
   id: '/Task',
   properties: {
     workerType: { type: 'string' },
-    schedule: { type: 'string' },
+    schedule: {
+      type: 'string',
+      format: 'cron',
+    },
     payload: { type: 'object, null' },
   },
   required: ['workerType', 'schedule'],
+};
+
+/**
+ * Add support for validation cron strings
+ *
+ * @param input - string to check
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+Validator.prototype.customFormats.cron = function (input: any): boolean {
+  if (typeof input === 'string') {
+    return isValidCron(input, {
+      seconds: true,
+      alias: true,
+    });
+  }
+
+  return false;
 };
 
 export default configSchema;
